cc4aa8fdf1e5691699ed0322d9732e797dd0fe54
MJ Aguelo <mjmaguelo@gmail.com>
2022-08-01 22:07:52 +0800

    styles: add colors from themes

--- src/components/layout/BaseLayout/BaseLayout.styles.tsx
+++ src/components/layout/BaseLayout/BaseLayout.styles.tsx
@@ -2,11 +2,12 @@ import {useMemo} from 'react';
 import {StyleSheet} from 'react-native';
 import {useTheme} from 'react-native-paper';
 
-const componentStyles = ({}: {colors: ReactNativePaper.ThemeColors}) =>
+const componentStyles = ({colors}: {colors: ReactNativePaper.ThemeColors}) =>
   StyleSheet.create({
     main: {
       display: 'flex',
       flex: 1,
+      backgroundColor: colors.background,
     },
     container: {
       margin: 16,
--- src/modules/landing/screens/Home/Home.screen.tsx
+++ src/modules/landing/screens/Home/Home.screen.tsx
@@ -1,11 +1,15 @@
 import CustomTypography from 'components/atoms/Typography/CustomTypography';
 import BaseLayout from 'components/layout/BaseLayout';
 import React, {FC} from 'react';
+import {useTheme} from 'react-native-paper';
 
 const HomeScreen: FC = () => {
+  const {colors} = useTheme();
   return (
     <BaseLayout>
-      <CustomTypography variant="h1">Home</CustomTypography>
+      <CustomTypography variant="h1" color={colors.text}>
+        Home
+      </CustomTypography>
     </BaseLayout>
   );
 };
