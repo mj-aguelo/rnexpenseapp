e5db3f44aa747c3385ed6680490bb1574bf9bbf8
MJ Aguelo <mjmaguelo@gmail.com>
2022-08-02 00:26:13 +0800

    chore: update app navigator

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -3,8 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from 'modules/landing/screens/Home';
 import useNavigationOptions from 'hooks/useNavigationOptions';
+import AddIncome from 'modules/creation/screens/AddIncome';
 
-const Stack = createStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  AddIncome: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 const Navigator = () => {
   const navigationOptions = useNavigationOptions();
@@ -12,11 +18,20 @@ const Navigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={navigationOptions}
-        />
+        <Stack.Group>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={navigationOptions}
+          />
+        </Stack.Group>
+        <Stack.Group screenOptions={{presentation: 'modal'}}>
+          <Stack.Screen
+            name="AddIncome"
+            component={AddIncome}
+            options={{title: 'Add Income', ...navigationOptions}}
+          />
+        </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   );
