f944f5ba60c33a1e9d0d132682537c1b343944a0
MJ Aguelo <mjmaguelo@gmail.com>
2022-08-02 01:36:10 +0800

    chore: rename text input folder

---
+++ src/components/atoms/CustomTextInput/CustomTextInput.tsx
@@ -0,0 +1,35 @@
+import React, {FC} from 'react';
+import {TextInput, useTheme} from 'react-native-paper';
+import {TextInputProps} from 'react-native-paper/lib/typescript/components/TextInput/TextInput';
+import useComponentStyles from './CustomTextInput.styles';
+
+export interface ICustomTextInput extends Partial<TextInputProps> {
+  theme?: ReactNativePaper.Theme;
+  value: any;
+}
+
+const CustomTextInput: FC<ICustomTextInput> = ({
+  onChangeText,
+  onBlur,
+  ...rest
+}) => {
+  const styles = useComponentStyles();
+  const theme = useTheme();
+
+  const handleOnChange = (text: string) => {
+    onChangeText && onChangeText(text);
+  };
+
+  return (
+    <TextInput
+      style={styles.main}
+      mode="outlined"
+      onChangeText={text => handleOnChange(text)}
+      onBlur={onBlur}
+      {...rest}
+      theme={theme}
+    />
+  );
+};
+
+export default CustomTextInput;
