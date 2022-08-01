5a60bf9ab41ec7e02483de18e2e025d614cd692b
MJ Aguelo <mjmaguelo@gmail.com>
2022-08-02 01:36:47 +0800

    chore: add types const

---
+++ src/constants/types.const.ts
@@ -0,0 +1,29 @@
+export const INCOME_TYPES = [
+  'salary',
+  'business',
+  'allowance',
+  'bonus',
+  'other',
+];
+
+export const EXPENSE_TYPES = [
+  'food',
+  'travel',
+  'home',
+  'misc',
+  'vehicle',
+  'electricity',
+  'water',
+  'pets',
+  'transpo',
+  'apparel',
+  'education',
+  'beauty',
+  'gift',
+  'health',
+  'gaming',
+  'gadgets',
+  'social',
+  'self-care',
+  'other',
+];
