f1a49580aa55ebc08361cf53587dbb341fa203aa
MJ Aguelo <mjmaguelo@gmail.com>
2022-08-04 04:16:39 +0800

    feature: add custom button

---
+++ src/components/atoms/CustomButton/CustomButton.tsx
@@ -0,0 +1,40 @@
+import {useTheme} from 'react-native-paper';
+import React, {FC, PropsWithChildren} from 'react';
+import {Button} from 'react-native-paper';
+import {ViewStyle} from 'react-native';
+
+export interface ICustomButtonProps extends PropsWithChildren {
+  color?: string;
+  labelColor?: string;
+  onPress?: Function;
+  style?: ViewStyle;
+  labelStyle?: ViewStyle;
+}
+
+const CustomButton: FC<ICustomButtonProps> = ({
+  children,
+  color,
+  labelColor,
+  onPress,
+  style,
+  labelStyle,
+}) => {
+  const {colors} = useTheme();
+  const defaultColor = color ?? colors.primary;
+  const defaultLabelColor = labelColor ?? colors.backdrop;
+
+  const handleOnPress = () => {
+    onPress && onPress();
+  };
+
+  return (
+    <Button
+      onPress={handleOnPress}
+      style={{...style, backgroundColor: defaultColor}}
+      labelStyle={{...labelStyle, color: defaultLabelColor}}>
+      {children}
+    </Button>
+  );
+};
+
+export default CustomButton;
