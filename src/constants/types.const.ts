188ef4c88e29f90820ff396629da2aed1110aad4
MJ Aguelo <mjmaguelo@gmail.com>
2022-08-04 04:17:40 +0800

    refactor: const types to follow keyvalue

--- src/constants/types.const.ts
+++ src/constants/types.const.ts
@@ -1,29 +1,105 @@
 export const INCOME_TYPES = [
-  'salary',
-  'business',
-  'allowance',
-  'bonus',
-  'other',
+  {
+    key: 'salary',
+    value: 'salary',
+  },
+  {
+    key: 'business',
+    value: 'business',
+  },
+  {
+    key: 'allowance',
+    value: 'allowance',
+  },
+  {
+    key: 'bonus',
+    value: 'bonus',
+  },
+  {
+    key: 'other',
+    value: 'other',
+  },
 ];
 
 export const EXPENSE_TYPES = [
-  'food',
-  'travel',
-  'home',
-  'misc',
-  'vehicle',
-  'electricity',
-  'water',
-  'pets',
-  'transpo',
-  'apparel',
-  'education',
-  'beauty',
-  'gift',
-  'health',
-  'gaming',
-  'gadgets',
-  'social',
-  'self-care',
-  'other',
+  {
+    key: 'food',
+    value: 'food',
+  },
+  {
+    key: 'food',
+    value: 'food',
+  },
+  {
+    key: 'travel',
+    value: 'travel',
+  },
+  {
+    key: 'home',
+    value: 'home',
+  },
+  {
+    key: 'misc',
+    value: 'misc',
+  },
+  {
+    key: 'vehicle',
+    value: 'vehicle',
+  },
+  {
+    key: 'electricity',
+    value: 'electricity',
+  },
+  {
+    key: 'water',
+    value: 'water',
+  },
+  {
+    key: 'pets',
+    value: 'pets',
+  },
+  {
+    key: 'transpo',
+    value: 'transpo',
+  },
+  {
+    key: 'apparel',
+    value: 'apparel',
+  },
+  {
+    key: 'education',
+    value: 'education',
+  },
+  {
+    key: 'beauty',
+    value: 'beauty',
+  },
+  {
+    key: 'gift',
+    value: 'gift',
+  },
+  {
+    key: 'health',
+    value: 'health',
+  },
+  {
+    key: 'gaming',
+    value: 'gaming',
+  },
+  {
+    key: 'gadgets',
+    value: 'gadgets',
+  },
+  {
+    key: 'social',
+    value: 'social',
+  },
+  {
+    key: 'self-care',
+    value: 'self-care',
+  },
+  {
+    key: 'other',
+    value: 'other',
+  },
 ];
